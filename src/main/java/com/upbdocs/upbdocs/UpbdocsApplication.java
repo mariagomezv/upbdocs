b44981b943b709aa6529cf140b923911307e25ce
mariagomezv <mariaj.gomezv@upb.edu.com>
2024-10-01 10:43:48 -0500

    first commit:create project

---
+++ src/main/java/com/upbdocs/upbdocs/UpbdocsApplication.java
@@ -0,0 +1,13 @@
+package com.upbdocs.upbdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbdocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upbdocs/upbdocs/UpbdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upbdocs.upbdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
